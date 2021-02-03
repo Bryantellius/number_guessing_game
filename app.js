fae68e82bd2484d24ac916e5c94cd19c7a38cb41
Ben <brbryant2639@gmail.com>
2021-02-02 22:06:46 -0600

    Completed base logic for game play. Polished resetGame function for tracking highscore.

--- app.js
+++ app.js
@@ -5,24 +5,30 @@ window.onload = function () {
   //   Variable Declarations for game
 
   let playing = false;
-  const history = [];
+  let history = [];
   let seconds = 0;
   let minutes = 0;
   let hours = 0;
-  let randomNumber = Math.floor(Math.random() * 100) + 1;
+  let attempts = 0;
+  let highscore = 0;
+  let randomNumber;
   const input = document.getElementById("guess");
   const feedbackP = document.getElementById("feedback");
   const historyP = document.getElementById("history");
   const timeP = document.getElementById("time");
   const btn = document.getElementById("guess-btn");
   const quitBtn = document.getElementById("quit-btn");
+  const highscoreP = document.getElementById("highscore");
+  const currentscoreP = document.getElementById("currentscore");
 
   // Event Listeners
 
   btn.addEventListener("click", startGame);
-  quitBtn.addEventListener("click", endGame);
+  quitBtn.addEventListener("click", resetGame);
 
   function startGame() {
+    // Assign random number to guess
+    randomNumber = Math.floor(Math.random() * 100) + 1;
     // Change playing status to true
     playing = true;
     // Change btn text
@@ -35,12 +41,6 @@ window.onload = function () {
 
     // Start game clock
     const duration = setInterval(clock, 1000);
-
-    function guess() {
-      if (playing) {
-        giveFeedback(input.value);
-      }
-    }
 
     function clock() {
       if (playing) {
@@ -56,28 +56,38 @@ window.onload = function () {
       } else if (hours > 24 || !playing) {
         clearInterval(duration);
       }
-      timeP.textContent = `${hours
+
+      currenttime = `${hours
         .toString()
         .padStart(2, "0")}:${minutes
         .toString()
         .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+      timeP.textContent = currenttime;
     }
   }
 
-  function endGame() {
+  function guess() {
     if (playing) {
-      playing = false;
-      btn.addEventListener("click", startGame);
-
-      btn.textContent = "Start";
-      input.disabled = true;
-      feedbackP.textContent = "Guess a whole number between 1 and 100";
-      historyP.textContent = "-";
-      timeP.textContent = "00:00:00";
-      seconds = 0;
-      minutes = 0;
-      hours = 0;
+      giveFeedback(input.value);
     }
+  }
+
+  function resetGame() {
+    playing = false;
+    btn.removeEventListener("click", guess);
+    btn.addEventListener("click", startGame);
+
+    btn.textContent = "Start";
+    input.disabled = true;
+    feedbackP.textContent = "Guess a whole number between 1 and 100";
+    historyP.textContent = "-";
+    timeP.textContent = "00:00:00";
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    attempts = 0;
+    currentscoreP.textContent = attempts;
+    history = [];
   }
 
   function giveFeedback(guess) {
@@ -109,6 +119,9 @@ window.onload = function () {
   }
 
   function checkCorrectAnswer(guess) {
+    attempts++;
+    currentscoreP.textContent = attempts;
+    
     if (guess > randomNumber) {
       feedbackP.textContent = `${guess} is too high. Try again`;
     } else if (guess < randomNumber) {
@@ -117,6 +130,11 @@ window.onload = function () {
       playing = false;
       feedbackP.textContent = `CONGRATS! ${guess} is the correct number.`;
       input.disabled = true;
+      if (highscore) {
+        highscore = highscore > attempts ? attempts : highscore;
+      } else highscore = attempts;
+
+      highscoreP.textContent = highscore;
     }
   }
 };
